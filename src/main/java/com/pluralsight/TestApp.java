9820bebe2d14f2b5a17cf21393d84b26e02ab006
ramenmac <kevinnguyen4200@gmail.com>
2026-04-15 15:31:49 -0500

    Generate random test scores, display, sort, display again for the love of it, make functions for the high,low,average then median

---
+++ src/main/java/com/pluralsight/TestApp.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+import java.util.Arrays;
+
+public class TestApp {
+    public static void main(String[] args) {
+        int[] testScores = new int[10];
+
+        generateTestsScores(testScores);
+        displayTestScores(testScores);
+        Arrays.sort(testScores);
+        displayTestScores(testScores);
+
+        displayHigh(testScores);
+        displayLow(testScores);
+        displayAverage(testScores);
+        displayMedian(testScores);
+    }
+
+    static void generateTestsScores(int[] testScores){
+
+        for (int i = 0 ; i < 10 ; i++) {
+            testScores[i] = (int)(Math.random() * 100) + 1;
+        }
+    }
+    static void displayTestScores (int[] testScores){
+        for (int i = 0 ; i < testScores.length ; i++){
+            System.out.printf("%d\t",testScores[i]);
+        }
+        System.out.printf("\n");
+    }
+    static void displayHigh(int[] testScores){
+        System.out.printf("Highest Score: %d\n", testScores[(testScores.length - 1)]);
+    }
+    static void displayLow(int[] testScores){
+        System.out.printf("Lowest Score: %d\n",testScores[0]);
+    }
+    static void displayAverage(int[] testScores){
+        float sum = 0;
+        for (int score : testScores){
+            sum += score;
+        }
+        sum /= testScores.length;
+        System.out.printf("Average Score: %.2f\n", sum);
+    }
+
+    static void displayMedian(int[] testScores){
+        float median = 0f;
+        median += testScores[(testScores.length / 2)];
+        median += testScores[(testScores.length / 2) - 1];
+        median /= 2f;
+        System.out.printf("Median Score: %.2f\n", median);
+
+    }
+}
